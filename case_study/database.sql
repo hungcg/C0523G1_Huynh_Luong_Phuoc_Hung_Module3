49a0feb41d7ae4bca4ebee1b5e4241541ba4f2da
hungcg <venenozelo94@gmail>
2023-08-10 21:26:12 +0700

    up

--- case_study/database.sql
+++ case_study/database.sql
@@ -177,26 +177,24 @@ values ("year"),("month"),("day"),("year");
 insert into loai_dich_vu(ten_loai_dich_vu)
 values ("Villa"),("House"),("Room");
 
+
 insert into dich_vu (ten_dich_vu,dien_tich,chi_phi_thue	,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,dien_tich_ho_boi,so_tang,ma_kieu_thue,ma_loai_dich_vu)
 values ("Villa Beach Front",25000,10000000,10,"vip","Có hồ bơi",500,4,3,1);
 
 insert into dich_vu (ten_dich_vu,dien_tich,chi_phi_thue,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,so_tang,ma_kieu_thue,ma_loai_dich_vu)
-values ("House Princess01"	,14000	,5000000	,7,"vip","Có thêm bếp nướng",3,2,2);
+values ("House Princess01",14000,5000000,7,"vip","Có thêm bếp nướng",3,2,2);
 
 insert into dich_vu (ten_dich_vu,dien_tich,chi_phi_thue	,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,ma_kieu_thue,ma_loai_dich_vu)
-values("Room Twin01"	,5000	,1000000,	2	,"normal"	,"Có tivi"	,4	,3);
-
+values("Room Twin01",5000,1000000,2,"normal","Có tivi",4,3);
 
 insert into dich_vu (ten_dich_vu,dien_tich,chi_phi_thue	,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,dien_tich_ho_boi,so_tang,ma_kieu_thue,ma_loai_dich_vu)
-values("Villa No Beach Front",	22000,	9000000,	8,	"normal",	"Có hồ bơi",	300,	3,	3,	1);
-
+values("Villa No Beach Front",22000,9000000,8,"normal","Có hồ bơi",	300,3,3,1);
 
 insert into dich_vu (ten_dich_vu,dien_tich,chi_phi_thue	,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,so_tang,ma_kieu_thue,ma_loai_dich_vu)
-values ("House Princess02"	,10000	,4000000	,5	,"normal",	"Có thêm bếp nướng"	,2	,3	,2);
-
+values ("House Princess02",10000,4000000,5,"normal","Có thêm bếp nướng",2,3,2);
 
 insert into dich_vu (ten_dich_vu,dien_tich,chi_phi_thue	,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,ma_kieu_thue,ma_loai_dich_vu)
-values ("Room Twin 02"	,3000	,900000,2	,"normal"	,"Có tivi",4	,3);
+values ("Room Twin 02",3000,900000,2,"normal","Có tivi",4,3);
 
 
 insert into dich_vu_di_kem ( ten_dich_vu_di_kem, gia, don_vi, trang_thai)
