bcb9db4ffc0d0026d4c23118e7a708e8587ed645
hungcg <venenozelo94@gmail>
2023-08-11 15:27:15 +0700

    up

--- case_study/task_2_task_5.sql
+++ case_study/task_2_task_5.sql
@@ -1,13 +1,38 @@
-/*Câu 2: Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là
-một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.*/
+-- Câu 2: Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là
+-- một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
 
 select * from nhan_vien where ho_ten like 'H%' or ho_ten like 'T%' or ho_ten like 'K%' and length(ho_ten) <= 15;
 
 
-/*Câu 3 :Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và
-có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị” */
+-- Câu 3 :Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và
+-- có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị” 
  
 select* from khach_hang
 where (select round (datediff(curdate(), ngay_sinh) / 365, 0))
 between 18 and 50
 and (dia_chi like '% Đà Nẵng' or dia_chi like '% Quảng Trị');
+
+
+-- Câu 4: Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu
+-- lần. Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của
+-- khách hàng. Chỉ đếm những khách hàng nào có Tên loại khách hàng là
+-- “Diamond”.
+
+select khach_hang.ma_khach_hang, khach_hang.ho_ten, count(hop_dong.ma_hop_dong) AS 'so_lan_dat_phong'
+from loai_khach join khach_hang on loai_khach.ma_loai_khach = khach_hang.ma_loai_khach
+join hop_dong on khach_hang.ma_khach_hang =hop_dong.ma_khach_hang
+where ten_loai_khach = 'Diamond'
+group by khach_hang.ma_khach_hang;
+
+
+-- Câu 5: Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong,
+-- ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien (Với
+-- tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng *
+-- Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem,
+-- hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. (những
+-- khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+
+
+
+
+
