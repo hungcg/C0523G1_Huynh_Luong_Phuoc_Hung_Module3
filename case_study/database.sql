ee8bb11865c63a8a329351b9336c51bd4fb1e637
hungcg <venenozelo94@gmail>
2023-08-11 11:25:24 +0700

    up

--- case_study/database.sql
+++ case_study/database.sql
@@ -188,7 +188,7 @@ insert into dich_vu (ten_dich_vu,dien_tich,chi_phi_thue	,so_nguoi_toi_da,tieu_ch
 values("Room Twin01",5000,1000000,2,"normal","Có tivi",4,3);
 
 insert into dich_vu (ten_dich_vu,dien_tich,chi_phi_thue	,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,dien_tich_ho_boi,so_tang,ma_kieu_thue,ma_loai_dich_vu)
-values("Villa No Beach Front",22000,9000000,8,"normal","Có hồ bơi",	300,3,3,1);
+values("Villa No Beach Front",22000,9000000,8,"normal","Có hồ bơi",300,3,3,1);
 
 insert into dich_vu (ten_dich_vu,dien_tich,chi_phi_thue	,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,so_tang,ma_kieu_thue,ma_loai_dich_vu)
 values ("House Princess02",10000,4000000,5,"normal","Có thêm bếp nướng",2,3,2);
---
+++ ss02_thiet_ke_db/mo_hinh_quan_he.sql
@@ -0,0 +1,72 @@
+create database mo_hinh_quan_he;
+
+use mo_hinh_quan_he;
+
+create table phieu_xuat(
+so_phieu_xuat int primary key auto_increment,
+ngay_xuat date
+);
+
+create table vat_tu(
+ma_vat_tu int primary key auto_increment,
+ten_vat_tu varchar(50)
+);
+
+create table chi_tiet_phieu_xuat(
+don_gia_xuat int,
+so_luong_xuat int,
+so_phieu_xuat int,
+foreign key(so_phieu_xuat) references phieu_xuat(so_phieu_xuat),
+ma_vat_tu int,
+foreign key (ma_vat_tu) references vat_tu (ma_vat_tu)
+);
+
+create table phieu_nhap (
+so_phieu_nhap int primary key auto_increment,
+ngay_nhap date
+);
+
+create table chi_tiet_phieu_nhap(
+don_gia_nhap int,
+so_luong_nhap int,
+so_phieu_nhap int,
+foreign key (so_phieu_nhap)  references phieu_nhap (so_phieu_nhap),
+ma_vat_tu int,
+foreign key(ma_vat_tu) references vat_tu (ma_vat_tu)
+);
+
+create table so_dien_thoai (
+ma_so_dien_thoai int primary key auto_increment,
+so_dien_thoai varchar(50)
+);
+
+create table nha_cung_cap(
+ma_nha_cung_cap int primary key auto_increment,
+ten_nha_cung_cap varchar(50),
+dia_chi varchar(50),
+ma_so_dien_thoai int,
+foreign key (ma_so_dien_thoai) references so_dien_thoai (ma_so_dien_thoai)
+);
+
+create table don_dat_hang(
+so_don_hang int primary key auto_increment,
+ngay_dat_hang date,
+ma_nha_cung_cap int,
+foreign key (ma_nha_cung_cap) references nha_cung_cap (ma_nha_cung_cap)
+);
+
+create table chi_tiet_don_dat_hang(
+ma_vat_tu int,
+foreign key (ma_vat_tu) references vat_tu(ma_vat_tu),
+so_don_hang int ,
+foreign key (so_don_hang) references don_dat_hang(so_don_hang)
+);
+
+
+
+
+
+
+
+
+ 
